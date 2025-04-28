0011210e1acbeb62c31173908ce9ccb1156d0811
Alejandro Botero <boterof.alejandro@gmail.com>
2025-04-28 15:55:42 -0500

    Delete repeated files

--- client/src/hooks/useUser.jsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { userContext } from "../context/userContext";
-
-// Hook to use UserContext
-const useUser = () => useContext(userContext);
-
-export default useUser;
