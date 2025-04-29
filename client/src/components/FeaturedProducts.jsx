684c46a972fe7389a6b13583e0cc4cda8c44280a
JanisCatalina10 <janiscatalinaa@gmail.com>
2025-04-28 22:28:06 -0400

    bootstrap en home

--- client/src/components/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts.jsx
@@ -1,24 +1,25 @@
 import { ProductCard } from "./ProductCard";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
 
 const FeaturedProducts = () => {
   // array vacío por ahora
   const featured = [];
 
   return (
-    <section className="featured-products">
-      <h2>Featured Products</h2>
-      <div className="product-carousel">
+    <section className="featured-products my-5">
+      <h2 className="text-center mb-4">Featured Products</h2>
+      <Row>
         {featured.length === 0 ? (
-          <p>aún no hay productos</p>
+          <p className="text-center">Aún no hay productos</p>
         ) : (
           featured.map((product) => (
-            <ProductCard
-              key={product.id}
-              product={product}
-            />
+            <Col key={product.id} md={4} className="mb-4">
+              <ProductCard product={product} />
+            </Col>
           ))
         )}
-      </div>
+      </Row>
     </section>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Hero from "../components/Hero";
 import FeaturedProducts from "../components/FeaturedProducts";
+import Container from "react-bootstrap/esm/Container"
 
 const Home = () => {
   return (
-    <>
+    <Container className="my-4">
       <Hero />
       <FeaturedProducts />
-    </>
+    </Container>
   );
 };
+
 export default Home;
