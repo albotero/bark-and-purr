0c140467f7df900f98bcdec89ca2462495340c88
JanisCatalina10 <janiscatalinaa@gmail.com>
2025-04-28 22:31:19 -0400

    bootstrap en footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,19 @@
+import Container from "react-bootstrap/esm/Container"
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+
 const Footer = () => {
   return (
-    <footer className="footer">
-      <p>© 2025 Bark & Purr. All rights reserved.</p>
+    <footer className="footer text-white py-3">
+      <Container>
+        <Row className="justify-content-center">
+          <Col className="text-center">
+            <p className="mb-0">© 2025 Bark & Purr. All rights reserved.</p>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 };
+
 export default Footer;
