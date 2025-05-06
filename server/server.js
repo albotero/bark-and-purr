7d1942de1063aa57af31f17e0230af668e95f814
JanisCatalina10 <janiscatalinaa@gmail.com>
2025-05-05 23:01:37 -0400

    favorites y avatar

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from "cors"
 import envs from "./config/envs.js"
 import authRoutes from "./src/routes/auth.routes.js"
 import productRoutes from "./src/routes/products.routes.js"
+import favoritesRoutes from "./src/routes/favorites.routes.js"
+import usersRoutes from "./src/routes/user.routes.js"
 
 const whiteList = [envs.SERVER_URL, envs.CLIENT_URL]
 const port = envs.PORT || 3000
@@ -20,5 +22,7 @@ app.use(
 app.use(express.json())
 app.use("/auth", authRoutes)
 app.use("/api", productRoutes)
+app.use('/favorites', favoritesRoutes)
+app.use('/users', usersRoutes)
 
 app.listen(port, () => console.log(`Listening on Port ${port}`))
