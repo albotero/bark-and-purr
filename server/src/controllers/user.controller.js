ac21d16e32f9c41909dbcd2dca2942a66b1d273b
JanisCatalina10 <janiscatalinaa@gmail.com>
2025-05-05 23:02:34 -0400

    favorite y avatar

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -11,18 +11,18 @@ export const updateAvatar = async (req, res) => {
     if (!file) return res.status(400).json({ error: 'No se subió ninguna imagen' })
   
     try {
-      // 1. Optimizar imagen con Sharp
+      //  Optimizar imagen con Sharp
       const optimizedImage = await sharp(file.buffer)
-        .resize(300) // Puedes ajustar tamaño
-        .jpeg({ quality: 80 }) // O .png() si prefieres
+        .resize(300) 
+        .jpeg({ quality: 80 }) 
         .toBuffer()
   
-      // 2. Subir a Cloudinary usando stream
+      // Subir a Cloudinary usando stream
       const streamUpload = (buffer) => {
         return new Promise((resolve, reject) => {
           const stream = cloudinary.uploader.upload_stream(
             {
-              folder: 'avatars', // opcional
+              folder: 'avatars', 
               resource_type: 'image'
             },
             (error, result) => {
@@ -36,7 +36,7 @@ export const updateAvatar = async (req, res) => {
   
       const result = await streamUpload(optimizedImage)
   
-      // 3. Guardar solo URL y public_id en la DB
+      //  Guardar solo URL y public_id en la DB
       const avatar_url = result.secure_url
       const avatar_key = result.public_id
   
