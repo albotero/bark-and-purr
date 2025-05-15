2df5b56efdd92c1a9d93a574478a5dfb823f093d
JanisCatalina10 <janiscatalinaa@gmail.com>
2025-05-15 16:57:24 -0400

    favorites agregado

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,19 @@
 import "./styles/bs-overrides.scss"
 import "bootstrap/dist/js/bootstrap.bundle.min"
 import "./styles/index.css"
+
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router"
+import { BrowserRouter } from "react-router-dom" // ojo: 'react-router-dom'
 import { I18nextProvider } from "react-i18next"
+
 import App from "./App.jsx"
 import i18n from "./utils/i18n.js"
+
 import UserProvider from "./context/UserContext"
 import { CartProvider } from "./context/CartContext"
 import ThemeProvider from "./context/ThemeContext.jsx"
+import { FavoritesProvider } from "./context/FavoritesContext.jsx" 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -17,9 +21,11 @@ createRoot(document.getElementById("root")).render(
       <ThemeProvider>
         <UserProvider>
           <CartProvider>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
+            <FavoritesProvider> {/* favorite context*/}
+              <BrowserRouter>
+                <App />
+              </BrowserRouter>
+            </FavoritesProvider>
           </CartProvider>
         </UserProvider>
       </ThemeProvider>
--- server/src/middlewares/validateFavorite.js
+++ server/src/middlewares/validateFavorite.js
@@ -1,9 +1,9 @@
 export const validateFavorite = (req, res, next) => {
-    const { user_id, product_id } = req.body;
-    
-    if (!user_id || !product_id) {
-        return res.status(400).json({ error: 'User ID and Product ID are required' });
-    }
-  
-    next();
-}
+  const { product_id } = req.body;
+
+  if (!product_id) {
+    return res.status(400).json({ error: "Product ID is required" });
+  }
+
+  next();
+};
