9a2f738f5907c4389bbab2bb7e291397c8181e24
Alejandro Botero <boterof.alejandro@gmail.com>
2025-05-16 08:06:17 -0500

    Refactor useApi

--- client/src/hooks/useApi.jsx
+++ client/src/hooks/useApi.jsx
@@ -1,8 +1,8 @@
-import { useCallback } from "react";
-import axios from "axios";
+import { useCallback } from "react"
+import axios from "axios"
 
-const fetchRetries = 5;
-const baseUrl = "http://localhost:3000";
+const fetchRetries = 5
+const baseUrl = "http://localhost:3000"
 
 export const useApi = () => {
   /**
@@ -15,40 +15,35 @@ export const useApi = () => {
    * @param token Optional
    * @returns fetchedData
    */
-  const consumeApi = useCallback(
-    async ({ method = "GET", fullUrl, endpoint, query, body, token }) => {
-      let data, error;
+  const consumeApi = useCallback(async ({ method = "GET", fullUrl, endpoint, query, body, token }) => {
+    let data, error
 
-      // Try n times before sending an error
-      for (let n = 0; n <= fetchRetries && !data; n++) {
-        try {
-          const headers = {
-            "Content-Type": "application/json",
-            ...(token && { Authorization: `Bearer ${token}` }),
-          };
-          const queryStr =
-            query &&
-            Object.keys(query)
-              .map((key) => `${key}=${query[key]}`)
-              .join("&");
-          const url = fullUrl
-            ? `${baseUrl}${fullUrl}`
-            : `${baseUrl}/api/${endpoint}` + (queryStr ? `?${queryStr}` : "");
-
-          const res = await axios({ method, url, headers, data: body });
-          data = res.data;
-        } catch ({ request, response }) {
-          error =
-            (response && `fetch.${response.status}`) || // Server answered with error code
-            (request && "fetch.no_response") || // Couldn't reach server
-            "fetch.no_request"; // Couldn't set up the request
+    // Try n times before sending an error
+    for (let n = 0; n <= fetchRetries && !data; n++) {
+      try {
+        const headers = {
+          "Content-Type": "application/json",
+          Authorization: token ? `Bearer ${token}` : undefined,
         }
+        const queryStr =
+          query &&
+          Object.keys(query)
+            .map((key) => `${key}=${query[key]}`)
+            .join("&")
+        const url = fullUrl ? `${baseUrl}${fullUrl}` : `${baseUrl}/api/${endpoint}` + (queryStr ? `?${queryStr}` : "")
+
+        const res = await axios({ method, url, headers, data: body })
+        data = res.data
+      } catch ({ request, response }) {
+        error =
+          (response && `fetch.${response.status}`) || // Server answered with error code
+          (request && "fetch.no_response") || // Couldn't reach server
+          "fetch.no_request" // Couldn't set up the request
       }
+    }
 
-      return { ...data, error };
-    },
-    []
-  );
+    return { ...data, error }
+  }, [])
 
-  return [consumeApi];
-};
+  return [consumeApi]
+}
