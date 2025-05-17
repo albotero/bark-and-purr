7323252576eebc5093c46ade9cdc5a90721d0621
Alejandro Botero <boterof.alejandro@gmail.com>
2025-05-17 12:03:52 -0500

    Add .env DB_PORT

--- server/config/db/connection.db.js
+++ server/config/db/connection.db.js
@@ -1,12 +1,13 @@
 import pg from "pg"
 import envs from "../envs.js"
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = envs
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = envs
 
 export default new pg.Pool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
+  port: DB_PORT,
   allowExitOnIdle: true,
 })
