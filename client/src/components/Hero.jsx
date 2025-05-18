e2ad61acb72654152a0ea6389918304f360ff1d7
Tamara Zapata <tamara.zapatagomez@gmail.com>
2025-05-18 11:31:14 -0400

    change on hero img

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,3 @@
-import Container from "react-bootstrap/esm/Container";
-import Row from "react-bootstrap/esm/Row";
-import Col from "react-bootstrap/esm/Col";
 import "../styles/Hero.css";
 import { useTranslation } from "react-i18next";
 
@@ -8,16 +5,21 @@ const Hero = () => {
   const { t } = useTranslation("home");
 
   return (
-    <section className="hero bg-primary text-white py-5">
-      <Container>
-        <Row className="text-center">
-          <Col>
-            <h1 className="display-4">{t("hero.title")}</h1>
-            <p className="lead">{t("hero.subtitle")}</p>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+    <div
+      className="hero"
+      style={{
+        position: "relative",
+        width: "100vw",
+        marginLeft: "calc(-50vw + 50%)",
+        overflow: "hidden",
+        lineHeight: 0,
+      }}
+    >
+      <div className="text-center text-white px-3">
+        <h1>{t("hero.title")}</h1>
+        <p>{t("hero.subtitle")}</p>
+      </div>
+    </div>
   );
 };
 
